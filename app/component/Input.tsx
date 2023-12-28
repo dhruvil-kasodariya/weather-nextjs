bc35e07b694544c10203ebdd6af5dc76f711ef22
dhruvil-kasodariya <kasodariyadhruvil45@gmail.com>
2023-12-29 00:26:03 +0530

    add search input in navbar and api intergation of weather api (get)

---
+++ app/component/Input.tsx
@@ -0,0 +1,28 @@
+"use client"
+import { FaSearchLocation } from "react-icons/fa";
+
+interface InputProps{
+    handleSearch:(event:React.KeyboardEvent<HTMLInputElement>)=>void
+    setLocation: React.Dispatch<React.SetStateAction<string>>
+    location:string
+}  
+
+const Input = ({handleSearch,setLocation,location}:InputProps) => {
+  return (
+    <form className="flex items-center md:w-2/4 w-full order-2 md:order-1">
+        <input
+        type="text"
+        placeholder="Search City" 
+        className="w-full bg-transparent border-b-2 placeholder-white outline-none text-white"
+        onKeyDown={handleSearch}
+        value={location}
+        onChange={(e)=>setLocation(e.target.value)}
+        />
+        <div className="ml-[-25px] text-white cursor-pointer">
+            <FaSearchLocation onClick={handleSearch}/>
+        </div>
+    </form>
+  )
+}
+
+export default Input
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,89 @@
+"use client"
+import React,  { useState } from "react"
 
+import Input from "./component/Input"
 
 export default function Home() {
+  const [data,setData] =useState({});
+  const [location,setLocation] =useState("");
+  const [error,setError] =useState("");
+
+  const url =`http://api.weatherapi.com/v1/forecast.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=${location}&days=7&aqi=yes&alerts=yes`
+
+  const handleSearch = async (e: React.KeyboardEvent<HTMLInputElement> | React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+    if (e.type === 'keydown' && (e as React.KeyboardEvent<HTMLInputElement>).key !== 'Enter') {
+      return;
+    }
+  
+    e.preventDefault();
+  
+    try {
+      const response = await fetch(url);
+  
+      if (!response.ok) {
+        throw new Error();
+      }
+  
+      const data = await response.json();
+      setData(data);
+      setLocation("");
+      setError("");
+    } catch (error) {
+      setError("City not found");
+      setData({});
+    }
+  };
+
+  // const handleSearch= async(e:React.KeyboardEvent<HTMLInputElement>)=>{
+  //   if(e.key === 'Enter'){
+  //     e.preventDefault();
+  //     try{
+  //       const response =await fetch(url);
+  //       if(!response.ok){
+  //         throw new Error()
+  //       }
+  //       const data =await response.json();
+  //       setData(data);
+  //       setLocation("");
+  //       setError('')
+  //     } catch(error){
+  //       setError("City not found");
+  //       setData({});
+  //     }
+  //   }
+  // }
+
+  let content;
+  if(Object.keys(data).length===0 && error === ""){
+    content =(
+      <div>
+        <h2> Welcome to the weather app</h2>
+      </div>
+    )
+  }else if(error!==""){
+    content =(
+      <div>
+        <p>City Not Found</p>
+        <p>Enter a Valid City Name</p>
+      </div>
+    )
+  } else {
+    content =(
+      <div>
+       {data.current.temp_c}
+      </div>
+    )
+  }
+
   return (
-    <div>Weather</div>
+    <div className="bg-cover bg-gradient-to-r from-blue-500 to-blue-300 h-fit">
+      <div className="bg-white/25 w-full flex flex-col h-fit">
+        <div className="flex flex-col md:flex-row justify-between items-center p-12">
+          <Input handleSearch={handleSearch} setLocation ={setLocation} location={location}/>
+          <h1 className="mb-8 md:mb-0 order-1 text-white py-2 px-4 rounded-xl italic font-bold">Weather App</h1>
+        </div>
+        {content}
+      </div>
+    </div>
   )
 }
