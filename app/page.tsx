ccac69fe909bee5cadd4ffbbecb74251b99d41c9
dhruvil-kasodariya <kasodariyadhruvil45@gmail.com>
2023-12-29 14:51:03 +0530

    change in css

--- app/page.tsx
+++ app/page.tsx
@@ -67,8 +67,8 @@ export default function Home() {
   }
 
   return (
-    <div className="bg-cover bg-gradient-to-r from-blue-500 to-blue-300 h-screen">
-      <div className="bg-white/25 w-full flex flex-col h-screen">
+    <div className="bg-cover bg-gradient-to-r from-blue-500 to-blue-300 h-fit">
+      <div className="bg-white/25 w-full flex flex-col h-fit">
         <div className="flex flex-col md:flex-row justify-between items-center p-12">
           <Input handleSearch={handleSearch} setLocation ={setLocation} location={location}/>
           <h1 className="mb-8 md:mb-0 order-1 text-white py-2 px-4 rounded-xl italic font-bold">Weather App</h1>
