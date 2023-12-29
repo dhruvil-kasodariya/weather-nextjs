9dad85a5181ddde6539721561bc96e0336d6f2e3
dhruvil-kasodariya <kasodariyadhruvil45@gmail.com>
2023-12-29 14:28:25 +0530

    include commonet like weekforecast and weather deatils and applied css on error messes and welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 "use client"
 import React,  { useState } from "react"
-
-import Input from "./component/Input"
-
+import Input from "@/app/component/Input"
+import Current from '@/app/component/Current'
+import WeatherDeatils from "@/app/component/WeatherDeatils"
+import WeekForecast from "@/app/component/WeekForecast"
+import suratData from '@/data.json';
 export default function Home() {
   const [data,setData] =useState({});
   const [location,setLocation] =useState("");
@@ -24,8 +26,9 @@ export default function Home() {
         throw new Error();
       }
   
-      const data = await response.json();
-      setData(data);
+      const jsonData = await response.json();
+      setData(jsonData);
+    
       setLocation("");
       setError("");
     } catch (error) {
@@ -33,51 +36,39 @@ export default function Home() {
       setData({});
     }
   };
-
-  // const handleSearch= async(e:React.KeyboardEvent<HTMLInputElement>)=>{
-  //   if(e.key === 'Enter'){
-  //     e.preventDefault();
-  //     try{
-  //       const response =await fetch(url);
-  //       if(!response.ok){
-  //         throw new Error()
-  //       }
-  //       const data =await response.json();
-  //       setData(data);
-  //       setLocation("");
-  //       setError('')
-  //     } catch(error){
-  //       setError("City not found");
-  //       setData({});
-  //     }
-  //   }
-  // }
-
+ 
   let content;
   if(Object.keys(data).length===0 && error === ""){
     content =(
-      <div>
-        <h2> Welcome to the weather app</h2>
+      <div className="text-white text-center h-full mt-[5rem]">
+        <h2 className="text-3xl font-bold mb-4"> Welcome to the weather app</h2>
+        <p className="text-xl ">Enter a city name to get the weather</p>
       </div>
     )
   }else if(error!==""){
     content =(
-      <div>
-        <p>City Not Found</p>
-        <p>Enter a Valid City Name</p>
+      <div className="text-white text-center h-full mt-[5rem] mb-10">
+        <p className="text-3xl font-bold mb-4">City Not Found</p>
+        <p className="text-xl">Enter a Valid City Name</p>
       </div>
     )
   } else {
     content =(
-      <div>
-       {data.current.temp_c}
+      <>
+      <div className="flex md:flex-row flex-col p-12 items-center justify-between">
+       <Current data={data}/>
+       <WeekForecast data={data}/>
       </div>
+      <div>
+        <WeatherDeatils data={data} />
+      </div>
+      </>
     )
   }
 
   return (
-    <div className="bg-cover bg-gradient-to-r from-blue-500 to-blue-300 h-fit">
-      <div className="bg-white/25 w-full flex flex-col h-fit">
+    <div className="bg-cover bg-gradient-to-r from-blue-500 to-blue-300 h-screen">
+      <div className="bg-white/25 w-full flex flex-col h-screen">
         <div className="flex flex-col md:flex-row justify-between items-center p-12">
           <Input handleSearch={handleSearch} setLocation ={setLocation} location={location}/>
           <h1 className="mb-8 md:mb-0 order-1 text-white py-2 px-4 rounded-xl italic font-bold">Weather App</h1>
